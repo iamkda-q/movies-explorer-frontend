694eb5b5ecf2077befa786d04777150c307ccfe9
KDA <iamkda@yandex.ru>
2022-06-08 01:43:33 +0300

    feat: add adaptive for moviesPage

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,4 +1,5 @@
 // import { Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import "./MoviesCard.css";
 import image from "../../assets/images/1.png";
 
@@ -10,10 +11,14 @@ function MoviesCard() {
                 <p className="movies__name">33 слова о дизайне</p>
                 <span className="movies__duration">1ч 17м</span>
             </div>
-            <button className="movies__saved"></button>
-            <button className="movies__save">Сохранить</button>
+            <button className="movies__delete"></button>
+            <Route path="/movies" exact>
+                <button className="movies__save app__link-button-type">
+                    Сохранить
+                </button>
+            </Route>
         </div>
-    )
+    );
 }
 
 export default MoviesCard;
